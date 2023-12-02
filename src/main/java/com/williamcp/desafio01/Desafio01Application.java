44b2f272ad40feb63e663d150d886c41035ff79c
William da Cruz Pires <williamcruzpires@alunos.utfpr.edu.br>
2023-12-01 22:34:26 -0300

    Implementação Classe Principal

--- src/main/java/com/williamcp/desafio01/Desafio01Application.java
+++ src/main/java/com/williamcp/desafio01/Desafio01Application.java
@@ -11,12 +11,30 @@ import com.williamcp.desafio01.Entities.Order;
 import com.williamcp.desafio01.Services.OrderService;
 
 @SpringBootApplication
-public class Desafio01Application {
+public class Desafio01Application implements CommandLineRunner {
 
 	@Autowired
 	OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		final Scanner leitor = new Scanner(System.in);
+		Order order = new Order();
+
+		System.out.print("Código: ");
+		order.setCode(Integer.parseInt(leitor.nextLine()));
+
+		System.out.print("Preço: ");
+		order.setBasic(Double.parseDouble(leitor.nextLine()));
+
+		System.out.print("Desconto: ");
+		order.setDiscount(Double.parseDouble(leitor.nextLine()));
+		
+		System.out.println("Pedido Código " + order.getCode());
+		System.out.printf("Valor total: %.2f%n ", orderService.total(order));
 	}
 }
